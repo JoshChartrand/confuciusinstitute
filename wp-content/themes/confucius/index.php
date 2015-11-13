77042aa23273f86d00575f50e013f33757776a56
ravebabies <becca.baird4@gmail.com>
2015-11-12 20:32:50 -0500

    added colour, sizes

--- wp-content/themes/confucius/index.php
+++ wp-content/themes/confucius/index.php
@@ -3,31 +3,197 @@
     <section>
 
     		<!-- This is the wordpress loop. It grabs all of our "Posts" -->
-            <?php if (have_posts()) : while ( have_posts()) : the_post(); ?>
+            <div class = "buddha blocks">
+
+            <?php
+
+            $post_id = 1;
+            $queried_post = get_post($post_id);
+            ?>
                 
                 <div class="content-block row">
                 
-                    <div class="col-md-6 pull-right">
+                    <div class="col-md-6 pull-right rightCenter">
 
-                        <div class="page-header">
-                            <h1><?php the_title(); ?></h1>
+                        <div class>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
                         </div>
 
-                        <?php the_content(); ?>
+                        <p><?php echo $queried_post -> post_content; ?></p>
 
                     </div>
 
                 </div>
 
-            <?php endwhile; else: ?>
+             <?php
 
-                <div class="page-header">
-                    <h1><Uh-oh...</h1>
+            $post_id = 19;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-left leftCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class = "taoism blocks">
+
+             <?php
+
+            $post_id = 6;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-right rightCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
                 </div>
 
-                <p>Looks like this page has no content!</p>
+            
+            <?php
 
-            <?php endif; ?>
+
+            $post_id = 15;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-left leftCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class = "confucius blocks">
+
+            <?php
+
+            $post_id = 8;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-right rightCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+
+            <?php
+
+            $post_id = 13;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-left leftCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class = "legalism blocks">
+
+                <?php
+            $post_id = 10;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-right rightCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+
+                <?php
+
+            $post_id = 17;
+            $queried_post = get_post($post_id);
+            ?>
+                
+                <div class="content-block row">
+                
+                    <div class="col-md-6 pull-left leftCenter">
+
+                        <div>
+
+                            <h1><?php echo $queried_post -> post_title; ?></h1>
+
+                        </div>
+
+                        <p><?php echo $queried_post -> post_content; ?></p>
+
+                    </div>
+
+                </div>
+
+            </div>
 
     </section>
 
